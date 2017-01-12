ffb29d40ef20d83f97857d138a54f3089563d095
BitO BSessiOn <bitobsession@gmail.com>
2017-01-12 19:42:58 +0100

    TWRP/MultiROM params revision
    Kernel build from source

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,15 +23,21 @@ TARGET_CPU_VARIANT := krait
 TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
 
 TARGET_NO_BOOTLOADER := true
-TARGET_PREBUILT_KERNEL := device/moto/shamu/kernel
-TARGET_KERNEL_SOURCE :=
+
+# Prebuilt kernel
+#TARGET_PREBUILT_KERNEL := device/moto/shamu/kernel
+
+# Inline kernel building
+TARGET_KERNEL_CONFIG := omni_shamu_defconfig
+TARGET_KERNEL_SOURCE := kernel/moto/shamu
+BOARD_KERNEL_IMAGE_NAME := zImage-dtb
 
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000
 
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=shamu msm_rtb.filter=0x37 ehci-hcd.park=3 utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags utags.backup=/dev/block/platform/msm_sdcc.1/by-name/utagsBackup coherent_pool=8M
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=shamu msm_rtb.filter=0x37 ehci-hcd.park=3 utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags utags.backup=/dev/block/platform/msm_sdcc.1/by-name/utagsBackup coherent_pool=8M androidboot.selinux=permissive
 
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset BOARD_RAMDISK_OFFSET --tags_offset BOARD_KERNEL_TAGS_OFFSET
 
@@ -43,9 +49,12 @@ TARGET_NO_RPC := true
 TARGET_BOARD_INFO_FILE := device/moto/shamu/board-info.txt
 
 USE_OPENGL_RENDERER := true
-VSYNC_EVENT_PHASE_OFFSET_NS := 7500000
-SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
+VSYNC_EVENT_PHASE_OFFSET_NS := 2500000
+SF_VSYNC_EVENT_PHASE_OFFSET_NS := 0000000
 TARGET_USES_ION := true
+
+TARGET_HW_DISK_ENCRYPTION := true
+#TARGET_CRYPTFS_HW_PATH := device/moto/shamu/cryptfs_hw
 
 TARGET_TOUCHBOOST_FREQUENCY := 1500
 TARGET_USERIMAGES_USE_EXT4 := true
@@ -62,6 +71,8 @@ BOARD_NEEDS_LZMA_MINIGZIP := true
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
 TARGET_RECOVERY_FSTAB = device/moto/shamu/fstab.shamu
+# Ensure f2fstools are built
+TARGET_USERIMAGES_USE_F2FS := true
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/moto/shamu
 
@@ -69,22 +80,26 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/moto/shamu
 BOARD_SUPPRESS_SECURE_ERASE := true
 
 # TWRP
-TW_CUSTOM_THEME := device/moto/shamu/twres
+#TW_CUSTOM_THEME := device/moto/shamu/twres
 DEVICE_RESOLUTION := 1440x2560
+TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_L_CRYPTO := true
 BOARD_HAS_NO_REAL_SDCARD := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
+TW_EXTRA_LANGUAGES := true
 
 # MultiROM config. MultiROM also uses parts of TWRP config
+TARGET_RECOVERY_IS_MULTIROM := true
 #MR_DEVICE_SPECIFIC_VERSION := f
 MR_ALLOW_NKK71_NOKEXEC_WORKAROUND := true
 MR_INPUT_TYPE := type_b
 MR_INIT_DEVICES := device/moto/shamu/multirom/mr_init_devices.c
 MR_DPI := xxhdpi
 MR_DPI_FONT := 435
+MR_USE_MROM_FSTAB := true
 MR_FSTAB := device/moto/shamu/twrp.fstab
 MR_KEXEC_MEM_MIN := 0x20000000
 MR_KEXEC_DTB := true
@@ -97,4 +112,7 @@ MR_ENCRYPTION_SETUP_SCRIPT := device/moto/shamu/multirom/mr_cp_crypto.sh
 MR_USE_QCOM_OVERLAY := true
 MR_QCOM_OVERLAY_HEADER := device/moto/shamu/multirom/mr_qcom_overlay.h
 MR_QCOM_OVERLAY_CUSTOM_PIXEL_FORMAT := MDP_RGBX_8888
+MR_NO_KEXEC := 2
+
+
 
